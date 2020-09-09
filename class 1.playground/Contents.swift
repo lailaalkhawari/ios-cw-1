96e0bcfd900a67fa121c77cb7f3e2c3f36e79427
lailaalkhawari <70814104+lailaalkhawari@users.noreply.github.com>
2020-09-09 20:16:24 +0300

    homework 2 done !!!

---
+++ class 1.playground/Contents.swift
@@ -0,0 +1,20 @@
+let name = "ليلى الخواري "
+var hobby1 = "الرسم"
+var hobby2 = "التصوير"
+var age = 16
+var height = 1.57
+print("اسمي \(name) عمري \(age) احب \(hobby1) \(hobby2) طولي \(height)")
+
+//part2
+
+let friendName = "bodour"
+var friendHeight = 1.6
+var friendAge = 23
+var friendHobby1 = "coding"
+var friendHobby2 = "reading"
+var age_difference = (friendAge)-(age)
+
+//bonus
+
+var sharedHobbies = (hobby1)==(friendHobby1) || (hobby1)==(friendHobby2) || (hobby2)==(friendHobby1) || (hobby2)==(friendHobby2)
+print (sharedHobbies)
